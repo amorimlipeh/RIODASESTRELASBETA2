ebd248593a00a4da5cef8980ee3e6afdfdb26e66
SEU_NOME <SEU_EMAIL>
2026-04-18 11:52:12 -0300

    🔥 estrutura completa enterprise criada

--- server.js
+++
@@ -1,3 +0,0 @@
-const app = require('./server/app');
-const PORT = process.env.PORT || 3000;
-app.listen(PORT,'0.0.0.0',()=>console.log('🚀 RIOBETA2 ON '+PORT));
--- server/routes/mov.js
+++
@@ -1,5 +0,0 @@
-const r=require('express').Router();
-const db=require('../services/db');
-
-r.get('/',(req,res)=>res.json(db.read().movimentacoes));
-module.exports=r;
