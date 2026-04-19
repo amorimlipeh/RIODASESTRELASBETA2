f3857c5c1c4e43af8c993c44c412e3daf5954fb2
SEU_NOME <SEU_EMAIL>
2026-04-19 08:48:56 -0300

    FIX REAL: anti-cache + compatível com estrutura atual + correção rota

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+// ANTI CACHE GLOBAL
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
 const express = require('express');
 const path = require('path');
 
